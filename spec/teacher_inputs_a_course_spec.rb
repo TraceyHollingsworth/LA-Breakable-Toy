7d89cdb3ab09cc507345ce7664e96375a8b890ad
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2015-01-12 23:18:23 -0500

    added validation to course model, added error messages to new course page, and added a scenario to the create course test

--- spec/teacher_inputs_a_course_spec.rb
+++ spec/teacher_inputs_a_course_spec.rb
@@ -9,20 +9,29 @@ feature "Create a Course", %q(
   Acceptance Criteria
   [ ] I must be a user with a teacher role
   [X] I must enter a title
-  [ ] I can optionally enter a description
-  [X] If all fields are complete, I am told that
+  [X] I can optionally enter a description
+  [X] If all required fields are complete, I am told that
       my course has been saved.
-  [ ] If a field is incomplete, I am given an
+  [X] If a required field is incomplete, I am given an
       error message and brought back to the form.
 
 ) do
 
-  scenario "Teacher creates a course" do
+  scenario "teacher creates a course" do
     visit new_course_path
     fill_in "Title", with: "Algebra"
+    fill_in "Description", with: "Algebra is the foundation for all higher maths."
 
     click_button "Create Course"
 
     expect(page).to have_content("Course Created Successfully!")
+  end
+
+  scenario "teacher is given an error if they forget a title" do
+    visit new_course_path
+
+    click_button "Create Course"
+
+    expect(page).to have_content("Title can't be blank")
   end
 end
