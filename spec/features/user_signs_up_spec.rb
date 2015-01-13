c9c8ce9718ff812f59a2d0d2a6b90640f548834f
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2015-01-13 13:02:52 -0500

    user can sign up

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -10,9 +10,9 @@ feature "Create a Course", %q(
   [X] There is a link to 'Sign Up' on the homepage.
   [X] If I fill in my first name, last name, display_name, email, password, and password confirmation correctly,
       I am greeted with a confirmation message that my account has been created.
-  [ ] If the password and password confirmation fields do not match, I am given an error message.
-  [ ] If my email already exists in the database, I am given a message that tells me I have already registered.
-  [ ] If my email is not formatted correctly, I am given an error message.
+  [X] If the password and password confirmation fields do not match, I am given an error message.
+  [X] If my email already exists in the database, I am given a message that tells me I have already registered.
+  [X] If my email is not formatted correctly, I am given an error message.
 
 ) do
 
@@ -29,5 +29,58 @@ feature "Create a Course", %q(
     click_button "Sign Up"
 
     expect(page).to have_content "Welcome! You have signed up successfully."
+  end
+
+  scenario "user provides mismatched passwords" do
+    visit root_path
+    click_on "Sign Up"
+
+    fill_in "First Name", with: "Joe"
+    fill_in "Last Name", with: "Shmoe"
+    fill_in "Display Name", with: "Mr. Shmoe"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "supersecret"
+    fill_in "Password confirmation", with: "notsupersecret"
+    click_button "Sign Up"
+
+    expect(page).to have_content "Password confirmation doesn't match"
+  end
+
+  scenario "user provides incorrect email format" do
+    visit root_path
+    click_on "Sign Up"
+
+    fill_in "First Name", with: "Joe"
+    fill_in "Last Name", with: "Shmoe"
+    fill_in "Display Name", with: "Mr. Shmoe"
+    fill_in "Email", with: "user"
+    fill_in "Password", with: "supersecret"
+    fill_in "Password confirmation", with: "supersecret"
+    click_button "Sign Up"
+
+    expect(page).to have_content "Email is invalid"
+  end
+
+  scenario "email is already registered" do
+    existing_user = User.create(
+      first_name: "Joe",
+      last_name: "Shmoe",
+      display_name: "Mr. Shmoe",
+      email: "user@example.com",
+      password: "supersecret",
+    )
+
+    visit root_path
+    click_on "Sign Up"
+
+    fill_in "First Name", with: existing_user.first_name
+    fill_in "Last Name", with: existing_user.last_name
+    fill_in "Display Name", with: existing_user.display_name
+    fill_in "Email", with: existing_user.email
+    fill_in "Password", with: existing_user.password
+    fill_in "Password confirmation", with: existing_user.password
+    click_button "Sign Up"
+
+    expect(page).to have_content "Email has already been taken"
   end
 end
