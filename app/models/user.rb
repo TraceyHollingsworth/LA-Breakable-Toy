423c5ae2cbd31e35ee667819bc35ae1a492abcb1
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2015-01-15 13:09:18 -0500

    seeded database with roles,  updated role field for users, and moved flash notice into partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :courses
+  belongs_to :role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Role.create(name: "Student")
+Role.create(name: "Teacher")
+Role.create(name: "Other")
