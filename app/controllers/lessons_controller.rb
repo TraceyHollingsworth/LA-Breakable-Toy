37040cdd0ed82f19197a6dd69e733a82691808d9
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2015-01-21 14:35:54 -0500

    added home page picture, created form for creating a lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,28 @@
 class LessonsController < ApplicationController
 
-def index
-end
+  def index
+    @lessons = Lesson.order( "lessons.created_at DESC")
+  end
+
+  def show
+    @lesson = Lesson.find(params[:course_id])
+  end
+
+  def create
+    @course = Course.find(params[:course_id])
+    @lesson = @course.lessons.new(lesson_params)
+
+    if @lesson.save
+      redirect_to course_path(@course)
+    else
+      flash[:warning] = @lesson.errors.full_messages.join(".  ")
+      redirect_to course_path(@course)
+    end
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:date, :objective, :prework, :lecture, :postwork, :extra_help, :add_ons)
+  end
 end
